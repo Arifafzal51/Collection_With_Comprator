ecd169e79f2fb0f2f3e9cce1ea78353bfbd0a4f8
arif afzal <arif.afzal_cs20@gla.ac.in>
2021-12-13 19:16:19 +0530

    Initial commit

---
+++ src/Sorting.java
@@ -0,0 +1,14 @@
+class Mysorting implements Comparator <IntegerT>
+{
+    public int compare(Integer 1, Integer 2)
+        if (1>2)
+    {
+        return -1
+    }
+        if (1<2)
+    {
+        return +1;
+    }
+
+
+}
