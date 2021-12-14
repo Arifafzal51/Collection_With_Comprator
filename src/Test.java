e5e77ad8fb9108b4bc56823f3b88db103649e046
arif afzal <arif.afzal_cs20@gla.ac.in>
2021-12-15 00:40:37 +0530

    e

--- src/Test.java
+++ src/Test.java
@@ -2,11 +2,11 @@ import java.util.TreeSet;
 
 public class Test
 {
-    public static void main(String[] args) {
-        TreeSet<Integer>obj = new TreeSet<Integer>();
-        obj.add(20);
-        obj.add(30);
-       // obj.add("sssssssss");
+     public static void main(String[] args) {
+//        TreeSet<Integer>obj = new TreeSet<Integer>();
+//        obj.add(20);
+//        obj.add(30);
+//       // obj.add("sssssssss");
         obj.add(10);
         obj.add(5);
         System.out.println(obj);
